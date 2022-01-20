2b4a955fbe93c231ef8f277b903891468c655db8
Jimmy Ma <jimmy_ma@hipposcreen-nc.com>
2022-01-20 14:18:30 +0800

    init test page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,81 +2,49 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main className="flex flex-col items-center w-full flex-1 text-center bg-[url('https://fundmore.net/assets/img/frontpage/background-img-1.png')]">
+        <nav className="bg-white border-gray-200 w-[1200px] h-[100px] mt-[40px] rounded-full">
+          <div className="container flex flex-wrap items-center mx-auto h-full">
+            <a className="flex h-full ml-[50px] py-[20px]" href="#">
+              <img className="h-full w-full" src="https://fundmore.net/assets/img/frontpage/logo.png" alt="" />
+            </a>
+            <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
+              <ul className="flex flex-col justify-end mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                <li>
+                  <a href="#" className="block text-[14px] py-2 border-b border-gray-100 md:border-0">サービス概要</a>
+                </li>
+                <li>
+                  <a href="#" className="block text-[14px] py-2 border-b border-gray-100 md:border-0">機能紹介</a>
+                </li>
+                <li>
+                  <a href="#" className="block text-[14px] py-2 border-b border-gray-100 md:border-0">プラン</a>
+                </li>
+                <li>
+                  <a href="#" className="block text-[14px] py-2 border-b border-gray-100 md:border-0">お知らせ</a>
+                </li>
+                <li className="bg-[#1B7BCC]">
+                  <img src="https://fundmore.net/assets/img/frontpage/mail-icon.png" alt="" />
+                  <a href="#" className="block text-[12px] py-2 text-white border-b border-gray-100 md:border-0">お問い合わせ</a>
+                </li>
+                <li className="bg-[#FF65AA]">
+                  <img src="https://fundmore.net/assets/img/frontpage/member-icon.png" alt="" />
+                  <a href="#" className="block text-[12px] py-2 text-white border-b border-gray-100 md:border-0">ログイン・登録</a>
+                </li>
+                
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="text-white text-[16px] bg-[#1B7BCC]">マーケティングを持続する顧客管理システム</div>
+        <div className="text-[#1b7bcc] text-[74px]">クラウドファンディングを<br></br>成長の先駆けに</div>
+        <div className="text-[#555555] text-[16px">Fundmoreは、クラウドファンディングに特化したCRM顧客管理ステムです。<br></br>アンケート調査、チャットボット、追加購入(EC機能)、物流連携をワンストップでご提供し、<br></br>クラウドファンディングマーケティングをサポートします。</div>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
   )
 }
