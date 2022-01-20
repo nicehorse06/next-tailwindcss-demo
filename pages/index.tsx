f18cd1bf14c9a9e3866c1374c3a5477d4f212c05
Jimmy Ma <jimmy_ma@hipposcreen-nc.com>
2022-01-20 15:00:51 +0800

    start center the word

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,35 +9,32 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col items-center w-full flex-1 text-center bg-[url('https://fundmore.net/assets/img/frontpage/background-img-1.png')]">
-        <nav className="bg-white border-gray-200 w-[1200px] h-[100px] mt-[40px] rounded-full">
-          <div className="container flex flex-wrap items-center mx-auto h-full">
-            <a className="flex h-full ml-[50px] py-[20px]" href="#">
-              <img className="h-full w-full" src="https://fundmore.net/assets/img/frontpage/logo.png" alt="" />
+        <nav className="bg-white border-gray-200 w-[1200px] h-[100px] mt-[40px] rounded-full overflow-hidden">
+          <div className="container flex flex-wrap flex-col justify-end items-center mx-auto h-full">
+            <a className="flex h-full pl-[50px] py-[20px] w-2/12" href="#">
+              <img className="h-full" src="https://fundmore.net/assets/img/frontpage/logo.png" alt="" />
             </a>
-            <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
-              <ul className="flex flex-col justify-end mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-                <li>
-                  <a href="#" className="block text-[14px] py-2 border-b border-gray-100 md:border-0">サービス概要</a>
-                </li>
-                <li>
-                  <a href="#" className="block text-[14px] py-2 border-b border-gray-100 md:border-0">機能紹介</a>
-                </li>
-                <li>
-                  <a href="#" className="block text-[14px] py-2 border-b border-gray-100 md:border-0">プラン</a>
-                </li>
-                <li>
-                  <a href="#" className="block text-[14px] py-2 border-b border-gray-100 md:border-0">お知らせ</a>
-                </li>
-                <li className="bg-[#1B7BCC]">
-                  <img src="https://fundmore.net/assets/img/frontpage/mail-icon.png" alt="" />
-                  <a href="#" className="block text-[12px] py-2 text-white border-b border-gray-100 md:border-0">お問い合わせ</a>
-                </li>
-                <li className="bg-[#FF65AA]">
-                  <img src="https://fundmore.net/assets/img/frontpage/member-icon.png" alt="" />
-                  <a href="#" className="block text-[12px] py-2 text-white border-b border-gray-100 md:border-0">ログイン・登録</a>
-                </li>
-                
-              </ul>
+            <div className="flex flex-row justify-end items-center h-full w-10/12">
+              <div className="w-[12%]">
+                <a href="#" className="block text-[14px] border-b border-gray-100 md:border-0">サービス概要</a>
+              </div>
+              <div className="w-[12%]">
+                <a href="#" className="block text-[14px] border-b border-gray-100 md:border-0">機能紹介</a>
+              </div>
+              <div className="w-[12%]">
+                <a href="#" className="block text-[14px] border-b border-gray-100 md:border-0">プラン</a>
+              </div>
+              <div className="w-[12%]">
+                <a href="#" className="block text-[14px] border-b border-gray-100 md:border-0">お知らせ</a>
+              </div>
+              <div className="bg-[#1B7BCC] h-full w-[12%]">
+                <img src="https://fundmore.net/assets/img/frontpage/mail-icon.png" alt="" />
+                <a href="#" className="block text-[12px] py-2 text-white border-b border-gray-100 md:border-0">お問い合わせ</a>
+              </div>
+              <div className="bg-[#FF65AA] h-full w-[14%]">
+                <img src="https://fundmore.net/assets/img/frontpage/member-icon.png" alt="" />
+                <a href="#" className="block text-[12px] py-2 text-white border-b border-gray-100 md:border-0">ログイン・登録</a>
+              </div>
             </div>
           </div>
         </nav>
